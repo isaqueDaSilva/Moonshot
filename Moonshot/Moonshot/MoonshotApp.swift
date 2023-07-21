b2c831fa598435fba2b0e72c987655ed2a0316e5
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-21 20:44:34 -0300

    First commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Isaque da Silva on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
