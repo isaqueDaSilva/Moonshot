14a512688f907561c8d1fb3528e9897d9a106bfd
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-22 23:56:48 -0300

    !1

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,13 +7,60 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let age: Int
+    let adress: Adress
+}
+
+struct Adress: Codable {
+    let street: String
+    let number: Int
+    let city: String
+    let uf: String
+}
+
 struct ContentView: View {
+    @State private var name = ""
+    @State private var age = 0
+    @State private var adress = ""
+    
+    @State private var showingView = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Decode JSON", action: {
+                let input = """
+                {
+                    "name": "Isaque da Silva Machado",
+                    "age": 18
+                    "adress": {
+                        "street": Rua 6
+                        "number": 96
+                        "city": "Resende"
+                        "uf": "RJ"
+                    }
+                }
+                """
+                
+                let data = Data(input.utf8)
+                let decoder = JSONDecoder()
+                
+                if let user = try? decoder.decode(User.self, from: data) {
+                    name = user.name
+                    age = user.age
+                    adress = "Rua \(user.adress.street), numero \(user.adress.number), \(user.adress.city) - \(user.adress.uf)"
+                    
+                    showingView = true
+                }
+            })
+            
+            if showingView {
+                Text("Usuario")
+                Text("Nome: \(name)")
+                Text("Idade: \(age)")
+                Text("Endereço: \(adress)")
+            }
         }
         .padding()
     }
