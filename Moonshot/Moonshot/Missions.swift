9431fe5016608ed9cebfa37003e24a8ab49029b8
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-23 01:48:54 -0300

    Moonshot 1.0.1

---
+++ Moonshot/Moonshot/Missions.swift
@@ -0,0 +1,20 @@
+//
+//  Missions.swift
+//  Moonshot
+//
+//  Created by Isaque da Silva on 23/07/23.
+//
+
+import Foundation
+
+struct Missions: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
